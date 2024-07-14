37c72adf932419ac20b45869ccf6d8e908d5cbf0
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-07-14 13:06:18 +0000

    Deployment

--- src/screens/footer/index.tsx
+++ src/screens/footer/index.tsx
@@ -3,9 +3,7 @@ import Github from "@/assets/Github.svg";
 import LinkedIn from "@/assets/Linked.svg";
 import Twitter from "@/assets/Twitter.svg";
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <footer className='fixed bottom-6 z-40 w-full py-6 '>
         <div className='mx-auto w-full px-12 space-y-10'>
