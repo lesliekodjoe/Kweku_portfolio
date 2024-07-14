a15e0b642b8d77a9c9451c7a1e5e175f7c9b0acc
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-07-14 13:05:28 +0000

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Kweku_portfolio/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
