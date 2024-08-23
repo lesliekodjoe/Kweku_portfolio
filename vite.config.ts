09516a7fda857c2414d28c96fcdbc1e21f4dcf3f
KwekuYamoah <kwekuandyamoah@gmail.com>
2024-08-23 21:48:51 +0000

    Fixed errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kwekuyamoah.github.io/",
+  base: "https://kwekuyamoah.github.io/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
-  },
-  build: {
-    outDir: 'build', // Specify the output directory as 'build'
   },
 })
 
